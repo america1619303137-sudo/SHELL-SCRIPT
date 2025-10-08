b966d566d915c5ca5a66ee176d4c41fabbc15d14
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-10-08 14:55:23 +0530

    mang

---
+++ 4-variablestricks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1::hii re kaisa hai"
+echo "$PERSON2::thik hoo apna bata"
+echo "$PERSON1::kyaa karta hai ab"
+echo "$PERSON2::kuch nahi"
+echo "$PERSON1:: hi iam murarai singhji"
+echo "$PERSON2::kaisa hai re murari"
