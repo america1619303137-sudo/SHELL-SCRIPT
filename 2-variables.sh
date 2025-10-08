3c6d7b6dde804412007d4d8966caa36984268d75
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-10-08 14:34:57 +0530

    mang

--- 2-variables.sh
+++ 2-variables.sh
@@ -3,4 +3,5 @@ echo "ram::hii re kaisa hai"
 echo "sam::thik hoo apna bata"
 echo "ram::kyaa karta hai ab"
 echo "sam::kuch nahi"
-echo "mura":: hi iam murarai singhji
+echo "mura:: hi iam murarai singhji"
+echo "man::kaisa hai re murari"
