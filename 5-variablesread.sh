69c1a2eb648f780c832aeb7a1b77ef57c80690d4
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-10-08 15:13:22 +0530

    mang

--- 5-variablesread.sh
+++ 5-variablesread.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 echo "enter your username/name::"
 read USERNAME #shows the username
-read -s USERNAME1 #hides username
-echo "enterd username is $USERNAME"
-echo "entered username is $USERNAME1"
+echo "enterd username/name is $USERNAME"
