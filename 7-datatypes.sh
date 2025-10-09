c6d7cbb4a5634fad067e419740c0607946b19487
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-10-09 15:56:28 +0530

    mang

--- 7-datatypes.sh
+++ 7-datatypes.sh
@@ -2,7 +2,7 @@
 #sum of two numbers
 NUMBER1=$1
 NUMBER2=$2
-EXECUTED AT=($TIMESTAMP)
-TOTAL=(($NUMBER1+$NUMBER2))
+EXECUTED AT=$(TIMESTAMP)
 echo="the total is executed at::$EXECUTED AT"
+TOTAL=$(($NUMBER1+$NUMBER2))
 echo="the total of $NUMBER1 & $NUMBER2 is :: $TOTAL"
