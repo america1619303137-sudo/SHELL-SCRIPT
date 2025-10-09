0f79ba00c34ba52a03a3c604b95cf31005150768
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-10-09 16:04:15 +0530

    mang

--- 7-datatypes.sh
+++ 7-datatypes.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#sum of two numbers
 NUMBER1=$1
 NUMBER2=$2
 TIMESTAMP=$(date)
-echo="the SUM is executed at: $TIMESTAMP"
+echo "the SUM is executed at: $TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
-echo="the sum of $NUMBER1 & $NUMBER2 is: $TOTAL"
+echo "the sum of $NUMBER1 & $NUMBER2 is: $TOTAL"
