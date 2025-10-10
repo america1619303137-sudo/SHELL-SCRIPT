0aab62d7b70764afd9b198bbc0daad63a4f5959e
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-10-10 15:40:27 +0530

    mang

--- 11-installation-on conditions.sh
+++ 11-installation-on conditions.sh
@@ -7,7 +7,21 @@ then
 fi
 
 dnf install mysql -y
+if [ $? -ne 0 ]
+    echo "ERROR:: installation is FAILED"
+
+    exit 1
+    else
+    echo "SUCCES:: installation is success"
+
 dnf install git -y
+if [ $? -ne 0 ]
+    echo "ERROR:: installation is FAILED"
+
+    exit 1
+    else
+    echo "SUCCES:: installation is success"
+fi
 #how can you check previos programm is sucess or not in shell-script?
 # if the exit status is 0 it is success or else failed
 #to check exit status===> echo $?
