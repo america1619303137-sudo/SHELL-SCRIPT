8e881c75fe218e1c16179bbc36844be939531151
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-10-10 13:13:35 +0530

    mang

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,8 +2,8 @@
 # -gt,-lt,-ge,-le,-eq
 nu=$1
 if [ $no -gt 100 ]
-echo "the entered no is greater than 100"
+    echo "the entered no is greater than 100"
 else
-echo "the entered number is less than or equl to 100"
+    echo "the entered number is less than or equl to 100"
 
     
