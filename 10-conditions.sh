ad1bce7aa9c511b26337ab802d6d52fd00837a45
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-10-10 13:11:49 +0530

    mang

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,2 +1,14 @@
 #/bin/bash
+# -gt,-lt,-ge,-le,-eq
+#!/bin/bash
 
+nu=$1
+
+if [ "$nu" -gt 100 ]; then
+    echo "The entered number is greater than 100"
+else
+    echo "The entered number is less than or equal to 100"
+fi
+
+
+    
