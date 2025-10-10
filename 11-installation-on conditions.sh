c4640ac2e29123efebad136910b0bacec22ff2ae
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-10-10 15:34:46 +0530

    mang

--- 11-installation-on conditions.sh
+++ 11-installation-on conditions.sh
@@ -3,5 +3,11 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR::you should ave acces to sudo user"
+    exit 1
 fi
+
 dnf install mysql -y
+dnf install git -y
+#how can you check previos programm is sucess or not in shell-script?
+# if the exit status is 0 it is success or else failed
+#to check exit status===> echo $?
