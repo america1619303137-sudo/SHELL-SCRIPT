72848856f18ccc57830bff3414a12845d7bd5d83
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-10-11 19:59:40 +0530

    mang

--- 7-datatypes.sh
+++ 7-datatypes.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 NUMBER1=$1
 NUMBER2=$2
-LOGS_FOLDER="/var/log/shellscript-logs"
-LOGS_FILE=$(echo $0 | cut -d "." -f1)
-TIMEOFEXECUTION=$(date +%y-%m-%y-%H-%M-%S) #showws date and time of execution
-LOG_FILE-NAME="$LOGS_FOLDER/$LOGA_FILE-$TIMEOFEXECUTION.log"
 EXECUTEAT=$(date)
-echo -e "\e[36m the SUM is executed at: $EXECUTEAT"
+TIMEOFEXECUTION=$(date +%y-%m-%y-%H-%M-%S) #showws date and time
+echo -e "\e[31m the SUM is executed at: $EXECUTEAT"
 SUM=$(($NUMBER1+$NUMBER2))
 echo -e "\e[33m the sum of $NUMBER1 & $NUMBER2 is: $SUM"
 echo "code executed at : $TIMEOFEXECUTION"
