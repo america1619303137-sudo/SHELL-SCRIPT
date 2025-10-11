5afe4c5911abfb60f5d3cf50cf837a6496be12d7
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-10-11 15:02:48 +0530

    mang

--- 7-datatypes.sh
+++ 7-datatypes.sh
@@ -4,4 +4,4 @@ NUMBER2=$2
 EXECUTEAT=$(date)
 echo -e "\e[31m the SUM is executed at: $EXECUTEAT"
 SUM=$(($NUMBER1+$NUMBER2))
-echo -e "\e[39m the sum of $NUMBER1 & $NUMBER2 is: $SUM"
+echo -e "\e[33m the sum of $NUMBER1 & $NUMBER2 is: $SUM"
