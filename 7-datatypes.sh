231f9bbaaa74fc0d3144903154b32f244b05eb5f
Mangal Singh <mangalsingh2e4n@gmail.com>
2025-10-11 15:00:31 +0530

    mang

--- 7-datatypes.sh
+++ 7-datatypes.sh
@@ -2,6 +2,6 @@
 NUMBER1=$1
 NUMBER2=$2
 EXECUTEAT=$(date)
-echo "the SUM is executed at: $EXECUTEAT"
+echo -e "\e[31m ..the SUM is executed at: $EXECUTEAT"
 SUM=$(($NUMBER1+$NUMBER2))
 echo "the sum of $NUMBER1 & $NUMBER2 is: $SUM"
